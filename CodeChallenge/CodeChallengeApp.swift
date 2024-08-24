f1bf99a3b39859d98eb63bfbe6f1d0035cfec866
June <powerball701@gmail.com>
2024-08-24 12:41:23 -0700

    Initial Commit

---
+++ CodeChallenge/CodeChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeChallengeApp.swift
+//  CodeChallenge
+//
+//  Created by Youngjoon Kim on 8/24/24.
+//
+
+import SwiftUI
+
+@main
+struct CodeChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
